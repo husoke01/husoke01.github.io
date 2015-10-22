70cbff3109829303205176f0ae35788ba71ee1d6
Keegan Husom <husoke01@luther.edu>
2015-10-21 22:01:50 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,1 +1,28 @@
 addInput = function() {
+}
+litem = document.createElement("li");
+chbox = document.createElement("input");
+chbox.type = checkbox;
+litem.className = priority.value;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+doneTask = function() {
+  if() {
+    this.parentNode.classList.add("done");
+  } else {
+    this.parentNode.classList.remove("done");
+  }
+}
