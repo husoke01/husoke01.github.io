8a76e85fa87cbd8d063585e6a4e9cfa3d539120b
Keegan Husom <husoke01@luther.edu>
2015-10-22 15:00:32 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -8,14 +8,14 @@ var myPriority
 
 myBox = document.querySelector("#textbox");
 myPriority = document.querySelector("#priority");
-myUl = document.querySelector("tasklist")''
+myUl = document.querySelector("tasklist");
 myUl = appendChild("li");
 myLi = document.createElement("li");
 myLi.classList.add(myPriority.value);
 myLi.appendChild("myInput");
-myLi.appendChild("myText");
 myInput = document.createElement("INPUT");
 myInput.type = checkbox;
+myLi.appendChild("myText");
 myText = document.createTextNode(myBox.value);
 }
 
