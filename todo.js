2746bffd053b08a11963692b7c0988241f08af03
Keegan Husom <husoke01@luther.edu>
2015-10-23 13:25:01 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,17 +1,17 @@
 addInput = function(){
-//var priority = document.querySelector('#priority').option.className;
-var myLi = document.createTextNode(document.querySelector('#textbox').value);
-var listItem = document.createElement("li");
-var chk = document.createElement("input");
-chk.type = "checkbox";
-listItem.appendChild(chk);
-listItem.appendChild(myLi);
-//listitem.className = priority;
-listItem.className = document.querySelector('#priority').value;
-chk.onclick = completeTask;
-var myUl = document.querySelector('#tasklist');
-myUl.appendChild(listItem);
-localSave("tasklist");
+  //var priority = document.querySelector('#priority').option.className;
+  var myLi = document.createTextNode(document.querySelector('#textbox').value);
+  var listItem = document.createElement("li");
+  var chk = document.createElement("input");
+  chk.type = "checkbox";
+  listItem.appendChild(chk);
+  listItem.appendChild(myLi);
+  //listitem.className = priority;
+  listItem.className = document.querySelector('#priority').value;
+  chk.onclick = completeTask;
+  var myUl = document.querySelector('#tasklist');
+  myUl.appendChild(listItem);
+  localSave("tasklist");
 }
 completeTask = function(){
   if (this.checked) { 
